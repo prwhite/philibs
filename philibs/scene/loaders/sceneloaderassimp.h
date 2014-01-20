485a986f541ac5504fe78a6974a20bc06c79fc8b
Payton White <payton@prehiti.net>
2014-01-20 02:18:05 -0800

    Added assimp submodule and penciled in scene loader for it.

---
+++ philibs/scene/loaders/sceneloaderassimp.h
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////
+//
+//    class: assimp
+//
+/////////////////////////////////////////////////////////////////////
+
+#ifndef sceneloaderloaderassimp_h
+#define sceneloaderloaderassimp_h
+
+/////////////////////////////////////////////////////////////////////
+
+#include "sceneloaderbase.h"
+
+/////////////////////////////////////////////////////////////////////
+
+namespace loader {
+  
+/////////////////////////////////////////////////////////////////////
+
+class assimp :
+  public base
+{
+  public:
+    assimp();
+    virtual ~assimp();
+//     assimp(assimp const& rhs);
+//     assimp& operator=(assimp const& rhs);
+//     bool operator==(assimp const& rhs) const;
+    
+  protected:
+    
+  private:
+    
+
+  // interface from base
+  public:
+    virtual scene::node* load ( std::string const& fname );
+    
+  protected:
+    
+
+};
+
+/////////////////////////////////////////////////////////////////////
+
+} // end of namespace loader 
+
+#endif // sceneloaderloaderassimp_h
+
+
